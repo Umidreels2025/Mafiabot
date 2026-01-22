d61d11b7e942bd3ce285b7d29c63f8ffde0d29a9
Umidjon Mamadjanov <umidjonmamadjanov653@gmail.com>
2026-01-22 22:32:35 +0500

    Add configuration settings in config.php
    Add configuration for Telegram, Redis, timers, and minimum players.

---
+++ Config/config.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'telegram' => [
+        'token' => '8466367319:AAGW3YG1pPx3s7OS4vHrYyK26-jsBzvXMH8',
+        'api' => 'https://api.telegram.org/bot'
+    ],
+    'redis' => [
+        'host' => '127.0.0.1',
+        'port' => 6379,
+        'db'   => 0
+    ],
+    'timers' => [
+        'night' => 90,
+        'discussion' => 150,
+        'voting' => 120
+    ],
+    'min_players' => 4
+];
