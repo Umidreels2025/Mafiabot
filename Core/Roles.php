66116a394f6d672a071311086e505790cd896d6f
Umidjon Mamadjanov <umidjonmamadjanov653@gmail.com>
2026-01-22 22:35:43 +0500

    Create Roles class with game role constants

---
+++ Core/Roles.php
@@ -0,0 +1,14 @@
+<?php
+namespace Core;
+
+class Roles {
+ const MAFIA='mafia';
+ const KOMISSAR='komissar';
+ const DOCTOR='doctor';
+ const MANIAK='maniak';
+ const DON='don';
+ const ADVOCATE='advokat';
+ const LOVER='lover';
+ const SHERZAND='sherzand';
+ const CIVIL='civil';
+}
