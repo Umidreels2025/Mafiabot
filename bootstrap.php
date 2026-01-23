b9bb88143d211aeb46c717e41b7b0af8a3952d98
Umidjon Mamadjanov <umidjonmamadjanov653@gmail.com>
2026-01-23 18:16:57 +0500

    Add files via upload

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,15 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
 
 $config = require __DIR__ . '/Config/config.php';
+
+spl_autoload_register(function ($class) {
+    $class = str_replace('\\', '/', $class);
+    $file = __DIR__ . '/' . $class . '.php';
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+
 
 use Storage\RedisStore;
 use Core\Bot;
